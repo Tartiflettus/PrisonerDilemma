e7fddb2212b2a0324aa7219aa45d556c4adbbceb
viicky <victor.creusel@gmail.com>
2019-01-30 19:53:01 +0100

    Modified code to be PEP8 compliant

--- PrisonerDilemma.py
+++ PrisonerDilemma.py
@@ -14,10 +14,10 @@ class Configuration:
         self._neighbor = neighbor
         self._T = T
         
-    def setCell(self, i, val):
+    def set_cell(self, i, val):
         self._line[i] = val
         
-    def getLine(self):
+    def get_line(self):
         return self._line
         
     def payoff(self, i, j):
@@ -26,44 +26,42 @@ class Configuration:
         return a*b + self._T*(1 - a*b)*b
     
     def neighborhood(self, i):
-        if self._neighbor % 2 != 0: #continuous range
+        if self._neighbor % 2 != 0: # continuous range
             ans = list(range(i-self._neighbor//2, 1+i+self._neighbor//2))
         else:
             ans = list(range(i-self._neighbor//2, i)) + list(range(1 + i , 1 + i + self._neighbor//2))
         return [x % len(self._line) for x in ans]
-        
-    
-    def totalPayoff(self, i):
+
+    def total_payoff(self, i):
         neighborhood = self.neighborhood(i)
         return sum([self.payoff(i, j) for j in neighborhood])
-    
+
     def next(self):
         ans = Configuration(len(self._line), self._neighbor, self._T)
         ans._neighbor = self._neighbor
         ans._T = self._T
         ans._line = cp.copy(self._line)
         
-        payoffs = [self.totalPayoff(i) for i in range(len(self._line))]
-        #update if payoff > other payoffs
+        payoffs = [self.total_payoff(i) for i in range(len(self._line))]
+        # update if payoff > other payoffs
         for i in range(len(self._line)):
-            #[payoffs[x] for x in self.neighborhood()]
-            #compute max payoff and get its index
+            # [payoffs[x] for x in self.neighborhood()]
+            # compute max payoff and get its index
             m = -1
             imax = -1
             for j in self.neighborhood(i):
                 if payoffs[j] > m:
                     m = payoffs[j]
                     imax = j
-            #update cell state if its payoff is not best
+            # update cell state if its payoff is not best
             if payoffs[i] < m:
                 ans._line[i] = self._line[imax]
 
         return ans
-    
-    
+
 
 if __name__ == "__main__":
-    #test
+    # test
     c = Configuration(8, 4, 1.1)
     for i in range(len(c._line)):
         c._line[i] = 1
@@ -77,4 +75,4 @@ if __name__ == "__main__":
         c = c.next()
         print(c._line)
     print("")
-    print([c.totalPayoff(i) for i in range(len(c._line))])
+    print([c.total_payoff(i) for i in range(len(c._line))])
--- UI.py
+++ UI.py
@@ -8,25 +8,26 @@ Created on Wed Jan 23 17:29:50 2019
 import tkinter as tk
 import PrisonerDilemma as pd
 
+
 class UI:
     def __init__(self, width=16, height=10):
         self._win = tk.Tk()
-        #self._win.pack(fill=tk.BOTH, expand=True)
-        #paned windows
+        # self._win.pack(fill=tk.BOTH, expand=True)
+        # paned windows
         self._pantop = tk.Frame(self._win)
         self._pantop.pack(side=tk.TOP)
         self._panbot = tk.Frame(self._win)
         self._panbot.pack(side=tk.BOTTOM)
         
-        #next configuration button
-        btnNextConfig = tk.Button(self._pantop, text="NEXT", command=self.nextConfig)
-        btnNextConfig.pack()
+        # next configuration button
+        btn_next_config = tk.Button(self._pantop, text="NEXT", command=self.next_config)
+        btn_next_config.pack()
         
-        #simple label
+        # simple label
         lab = tk.Label(self._pantop, text="Most recent configuration")
         lab.pack()
         
-        #init the grid
+        # init the grid
         self._width = width
         self._height = height
         self._grid = [[None for i in range(width)] for j in range(height)]
@@ -38,36 +39,29 @@ class UI:
                 self._grid[y][x].grid(row=y, column=x)
                 
         self._win.mainloop()
-    
-    
-    
-    def retrieveConfig(self, y):
+
+    def retrieve_config(self, y):
         config = pd.Configuration(self._width, 2, 1.1)
         for x in range(self._width):
-            config.setCell(x, self._values[y][x].get())
+            config.set_cell(x, self._values[y][x].get())
         return config
     
-    
-    def setConfig(self, config):
-        line = config.getLine()
+    def set_config(self, config):
+        line = config.get_line()
         for i in range(len(line)):
             self._values[0][i].set(line[i])
-            
-            
-    def nextConfig(self):
+
+    def next_config(self):
         
-        futureConfig = self.retrieveConfig(0).next()
-        #shift configs towards the bottom
+        future_config = self.retrieve_config(0).next()
+        # shift configs towards the bottom
         for y in range(self._height-1, 0, -1):
             for x in range(0, self._width):
                 self._values[y][x].set(self._values[y-1][x].get())
-        #update the most recent config
-        self.setConfig(futureConfig)
-        #print(futureConfig.getLine())
-        
-        
-        
-        
+        # update the most recent config
+        self.set_config(future_config)
+
+
 if __name__ == "__main__":
     ui = UI()
         
